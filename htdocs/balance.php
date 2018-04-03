dbc15f72989a714321f89f8660897a75ec331baf
andreistalker <usadekall@gmail.com>
2018-04-03 19:49:59 +0300

    Update balance.php

--- htdocs/balance.php
+++ htdocs/balance.php
@@ -1,28 +1,6 @@
-<?php
-include_once('src/config.php');
-
-include_once('src/head.php');
-
-if(isset($_GET['balanceTop']))
-	if($_GET['balanceTop'] != 0)
-	{
-		$urldeposit = "https://steemconnect.com/sign/transfer?from=".$_COOKIE['username']."&to=".$botaccount."&amount=".$_GET['balanceTop']."%20SBD&memo=deposit&redirect_uri=".$websiteadress."/added.php";
-		$urlwithdraw = "https://steemconnect.com/sign/transfer?from=".$_COOKIE['username']."&to=".$botaccount."&amount=0.001%20SBD&memo=withdraw%20".$_GET['balanceTop']."&redirect_uri=".$websiteadress."/added.php";
-		if($_GET['action'] == "deposit")
-			header("Location: ".$urldeposit);
-		else 
-			header("Location: ".$urlwithdraw);
-		die();
-	}
-?>
 <html>
 	<head><title>SteemCasino - Top up balance</title></head>
 	<body>
-		How much would you like to <?php if($_GET['action'] == "deposit") echo "deposit"; else echo "withdraw"; ?>?
-		<form>
-			<input type="number" step=".001" min="0.001" pattern="\d+(\.\d{2})?" class="form-control" id="balanceTop" name="balanceTop">
-			<input type="hidden" name="action" value="<?php if($_GET['action'] == "deposit") echo "deposit"; else echo "withdraw"; ?>">
-			<input type="submit" value="Submit">
-		</form>
+		<h1>This website is in BETA TESTING. Deposits/Withdrawals are suspended.</h1>
 	</body>
 </html>
